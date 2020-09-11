70596792bd964c8a02dbc47654350915c3fe8927
Pat Doumont <pdoumont@gmu.edu>
2020-09-11 14:22:48 -0400

    First commit

---
+++ initials.py
@@ -0,0 +1,13 @@
+full_name = input('Enter your name separated by spaces:')
+names = full_name.strip().split()
+print(names)
+
+initials = []
+
+for name in names:
+	initials.append(name[0])
+
+print("Your initials are:", end=' ')
+for i in initials:
+	print(f'{i}', end='')
+print('')
